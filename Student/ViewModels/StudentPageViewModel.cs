1c3a59ebfba8952e5ebde9c7fbe67435bd117b63
Tu Ha <54472431+haminhtu12@users.noreply.github.com>
2020-08-20 11:09:31 +0700

    init

---
+++ Student/ViewModels/StudentPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.ViewModels
+{
+    public class StudentPageViewModel
+    {
+        Random rand = new Random();
+
+        public ObservableCollection<Model.ManagerStudent> Students { get; private set; }
+        = new ObservableCollection<Model.ManagerStudent>();
+
+        public StudentPageViewModel()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var item = new Model.ManagerStudent()
+                {
+                    id = i,
+                    name = string.Format("Task name {0}", i),
+                    gpa = 10*(float)this.rand.NextDouble()
+                };
+                this.Students.Add(item);
+            }
+
+        }
+    }
+}
